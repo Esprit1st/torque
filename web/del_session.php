d915599bdecc16d70a6b2336cb74295c5574e008
Esprit1st <39107090+Esprit1st@users.noreply.github.com>
2021-03-08 14:04:54 -0700

    Update del_session.php
    - session-table needs updating after a partial (selected range) session has been deleted

--- web/del_session.php
+++ web/del_session.php
@@ -25,6 +25,14 @@ if (isset($deletesession) && !empty($deletesession)) {
 		$delresult = mysqli_query($con, "DELETE FROM $db_sessions_table WHERE session=" . quote_value($deletesession) . " AND eml=" . quote_value($_SESSION["torque_eml"]) ) or die(mysqli_error($con));
 	}
 	$delresult = mysqli_query($con, "DELETE FROM $db_table_full WHERE session=" . quote_value($deletesession).$range . " AND user=" . quote_value($_SESSION["torque_userid"])) or die(mysqli_error($con));
+	//** update the session data in the sessions table
+	$userqry = mysqli_query($con, "SELECT MIN(time) AS start, MAX(time) AS end, COUNT(*) as size FROM $db_table_full WHERE session=" . quote_value($deletesession) ) or die(mysqli_error($con));
+	if (mysqli_num_rows($userqry) == 1) {
+		$row = mysqli_fetch_assoc($userqry);
+		//** now update session with new start, end, time
+		$userqry= "UPDATE $db_sessions_table SET `time` = ".quote_value($row["start"]).", `timestart` = ".quote_value($row["start"]).", `timeend` = ".quote_value($row["end"]).", `sessionsize` = ".quote_value($row["size"])." WHERE session=" . quote_value($deletesession);
+		mysqli_query($con, $userqry) or die(mysqli_error($con));
+	}
 }
 //echo "<!-- End del_session.php at ".date("H:i:s", microtime(true))." -->\r\n";
 ?>
